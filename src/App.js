3e2e67369e08c2a7fe35b9a0dc95c7ab7b2dfcfc
kei86 <61891585+kei86@users.noreply.github.com>
2022-01-01 15:32:13 +0900

    org

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
+
 import "./styles.css";
+
+const user = {
+  name: "KE",
+  image: "https://source.unsplash.com/Sg3XwuEpybU",
+  email: "12345ex.com",
+  phone: "090-0000000",
+  company: {
+    name: "TEST COMPANY"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -9,6 +22,7 @@ export default function App() {
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
