19a87f8a9a115461b37fcc2efc1651a9db345854
kei86 <61891585+kei86@users.noreply.github.com>
2022-01-01 15:53:06 +0900

    template

---
+++ src/components/atoms/layout/Footer.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+export const Footer = () => {
+  return <SFooter>&copy; 2021 test Inc.</SFooter>;
+};
+
+const SFooter = styled.footer`
+  background-color: #119993;
+  color: #fff;
+  text-align: center;
+  padding: 8px 0px;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+`;
